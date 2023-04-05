264a6eb5b8b3ce3166e933f9ea93499ae348950f
shantanu <pujarishantanu33@gmail.com>
2023-04-05 12:59:14 +0530

    Attempt

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,9 +1,12 @@
 package com.driver;
 
+import java.util.List;
+
 public class Email {
 
     private String emailId;
     private String password;
+
 
     public Email(String emailId){
         this.emailId = emailId;
@@ -24,6 +27,34 @@ public class Email {
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
-        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-    }
+        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special characte
+        if (oldPassword!=password){
+            return;
+        }
+        boolean uppercase =false;
+        boolean lowecase =false;
+        boolean digit = false;
+        boolean special = false;
+        if (newPassword.length()<8){
+            return;
+        }
+        for (int i=0;i<newPassword.length();i++){
+            char c = newPassword.charAt(i);
+            if (c>='A' && c<='Z'){
+                uppercase=true;
+            }
+            else if(c>='a' && c<='z'){
+                lowecase=true;
+            }
+            else if (c>=0 && c<=9){
+                digit=true;
+            }else{
+                special=true;
+            }
+        }
+        if(uppercase==true && lowecase==true && digit==true && special==true){
+            password=newPassword;
+        }
+
+                   }
 }
---
+++ src/main/java/com/driver/mail.java
@@ -0,0 +1,42 @@
+package com.driver;
+
+import java.util.Date;
+
+public class mail {
+    private Date date;
+    private String senderId;
+    private String message;
+
+    public mail() {
+    }
+
+    public mail(Date date, String senderId, String message) {
+        this.date = date;
+        this.senderId = senderId;
+        this.message = message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
